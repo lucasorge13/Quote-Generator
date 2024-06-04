356dc6ee2c0a40a2156312c5bc086cf072ddbdde
Lucas Sorge <lucasorge13@gmail.com>
2024-06-04 12:17:27 -0500

    final commit

--- src/script.js
+++ src/script.js
@@ -1,33 +1,39 @@
-const quoteText = document.querySelector(".quote"), 
-authorName = document.querySelector(".author .name"),
+const quoteText = document.querySelector(".quote"),
 quoteBtn = document.querySelector("button"),
-soundBtn = document.querySelector(".sound"),
+authorName = document.querySelector(".name"),
+speechBtn = document.querySelector(".speech"),
 copyBtn = document.querySelector(".copy"),
-twitterBtn = document.querySelector(".twitter");
+twitterBtn = document.querySelector(".twitter"),
+synth = speechSynthesis;
 
 function randomQuote(){
-  quoteBtn.classList.add("Loading");
-  quoteBtn.innerText = "Loading Quote...";
-  fetch("https://api.quotable.io/random").then(res => res.json()).then(result => { 
-    quoteText.innerText = result.content;
-    authorName.innerText = result.author;
-    quoteBtn.innerText = "New Quote";
-    quoteBtn.classList.remove("Loading");
-  });
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "New Quote";
+    });
 }
 
-soundBtn.addEventListener("click", ()=>{
-  let utterance = new SpeechSynthesisUtterance('${quoteText.innerText} by ${authorName.innerText}');
-  speechSynthesis.speak(utterance);
+speechBtn.addEventListener("click", ()=>{
+    if(!quoteBtn.classList.contains("loading")){
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        synth.speak(utterance);
+        setInterval(()=>{
+            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+        }, 10);
+    }
 });
 
 copyBtn.addEventListener("click", ()=>{
-  navigator.clipboard.writeText(quoteText.innerText)
+    navigator.clipboard.writeText(quoteText.innerText);
 });
 
 twitterBtn.addEventListener("click", ()=>{
-  let tweetUrl = 'https://twitter.com/intent/tweet?url=${quoteText.innerText}';
-  window.open(tweetUrl, "_blank");
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
 });
 
 quoteBtn.addEventListener("click", randomQuote);
